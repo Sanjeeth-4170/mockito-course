12a2ef976f5edc4bf92b7ab50feda63ecea7c9c4
sanjeeth <sanjeeth_ms@yahoo.com>
2021-03-28 17:52:22 +0530

    COMMITING MOCKITO COURSE

---
+++ src/test/java/com/in28minutes/business/TodoBusinessImplMockitoInjectMocksTest.java
@@ -0,0 +1,110 @@
+package com.in28minutes.business;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.in28minutes.data.api.TodoService;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TodoBusinessImplMockitoInjectMocksTest {
+	
+	//We can replace @RunWith(MockitoJUnitRunner.class) by using @Rule
+	//@Rule
+    //public MockitoRule mockitoRule = MockitoJUnit.rule();
+	
+	//TodoService todoService = mock(TodoService.class); can be replaced by using @RunWith(MockitoJUnitRunner.class) placed 
+	//on top of the class and by using @Mock
+	@Mock
+	TodoService todoService;
+
+	// TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService); can be replaced by using @InjectMocks
+	@InjectMocks
+	TodoBusinessImpl todoBusinessImpl;
+
+	//ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class); this can be replaced by using @Captor
+	@Captor
+	ArgumentCaptor<String> stringArgumentCaptor;
+
+	@Test
+	public void usingMockito() {
+		List<String> allTodos = Arrays.asList("Learn Spring MVC",
+				"Learn Spring", "Learn to Dance");
+
+		when(todoService.retrieveTodos("Ranga")).thenReturn(allTodos);
+
+		List<String> todos = todoBusinessImpl
+				.retrieveTodosRelatedToSpring("Ranga");
+		assertEquals(2, todos.size());
+	}
+
+	@Test
+	public void usingMockito_UsingBDD() {
+		List<String> allTodos = Arrays.asList("Learn Spring MVC",
+				"Learn Spring", "Learn to Dance");
+
+		//given
+		given(todoService.retrieveTodos("Ranga")).willReturn(allTodos);
+
+		//when
+		List<String> todos = todoBusinessImpl
+				.retrieveTodosRelatedToSpring("Ranga");
+
+		//then
+		assertThat(todos.size(), is(2));
+	}
+
+	@Test
+	public void letsTestDeleteNow() {
+
+		List<String> allTodos = Arrays.asList("Learn Spring MVC",
+				"Learn Spring", "Learn to Dance");
+
+		when(todoService.retrieveTodos("sanju")).thenReturn(allTodos);
+
+		todoBusinessImpl.deleteTodosNotRelatedToSpring("sanju");
+		
+		verify(todoService).retrieveTodos("sanju");
+
+		verify(todoService).deleteTodo("Learn to Dance");
+
+		verify(todoService, Mockito.never()).deleteTodo("Learn Spring MVC");
+
+		verify(todoService, Mockito.never()).deleteTodo("Learn Spring");
+
+		verify(todoService, Mockito.times(1)).deleteTodo("Learn to Dance");
+		// atLeastOnce, atLeast
+
+	}
+
+	@Test
+	public void captureArgument() {
+		List<String> allTodos = Arrays.asList("Learn Spring MVC",
+				"Learn Spring", "Learn to Dance");
+		Mockito.when(todoService.retrieveTodos("Ranga")).thenReturn(allTodos);
+
+		todoBusinessImpl.deleteTodosNotRelatedToSpring("Ranga");
+		Mockito.verify(todoService).deleteTodo(stringArgumentCaptor.capture());
+
+		assertEquals("Learn to Dance", stringArgumentCaptor.getValue());
+	}
+}
---
+++ src/test/java/com/in28minutes/business/TodoBusinessImplStubTest.java
@@ -0,0 +1,24 @@
+package com.in28minutes.business;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.in28minutes.data.api.TodoService;
+import com.in28minutes.data.stub.TodoServiceStub;
+
+public class TodoBusinessImplStubTest {
+
+	@Test
+	public void usingAStub() {
+		
+		//TodoServiceStub is implementation class of TodoService interface
+		TodoService todoService = new TodoServiceStub();
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+		List<String> todos = todoBusinessImpl
+				.retrieveTodosRelatedToSpring("Ranga");
+		assertEquals(2, todos.size());
+	}
+}
